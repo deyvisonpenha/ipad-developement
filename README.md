99d90a220cca92feb6352bbf158789cec9756466
Deyvison Penha <deyvisonpenha1@gmail.com>
2020-05-18 10:57:52 +0000

    prettier, repository and appointments

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ yarn add express
 
 ## iniciando o backend com TS
 instalando as dependencias como:
-express , typescript -D , @types/express -D, ts-node-dev -D
+express , typescript -D , @types/express -D, ts-node-dev -D, uuidv4, date-fns
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: "hello world"})
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
-    console.log('servidor iniciado')
-})
+  console.log('servidor iniciado');
+});
